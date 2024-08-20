8c05dcf42100525e0132b95db99f32c4448513d0
Nahidar Alam <nahidaralam20@gmail.com>
2024-08-20 13:08:04 +0600

    navbar added

---
+++ src/Navbar.tsx
@@ -0,0 +1,15 @@
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
